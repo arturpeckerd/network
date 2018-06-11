7195c5682c11151db1adcc334001c03bd2ba23a6
Carlos Garcia <arturpeckerd@gmail.com>
2018-06-10 21:55:33 -0500

    including git commands

--- script.sh
+++ script.sh
@@ -7,5 +7,8 @@ while :
 do
     echo Reading the public ip address and save it in log file
     curl https://checkmyip.com/ | grep dsl > log
+    git add log
+    git commit -m "updating ip"
+    git push -u origin master
     sleep 43200
 done
