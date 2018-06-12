c247535e9420c90d7b4aa9a5cb7e2c83f4b75587
Carlos Garcia <arturpeckerd@gmail.com>
2018-06-12 19:07:52 +0000

    updating for sabreauto

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 sleep 5
-cd /home/carlos/Projects/github
+cd /home/root/network
 
 while :
 do
     echo Reading the public ip address and save it in log file
-    curl https://checkmyip.com/ | grep dsl > log
+    curl -L checkmyip.com/ | grep dsl > log
     git add log
     git commit -m "updating ip"
     git push -u origin master
