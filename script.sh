dfed89ef2f648fcb0cff664fbd7f6e9d105355a2
Carlos Garcia <arturpeckerd@gmail.com>
2018-06-27 01:35:58 +0000

    Updating script file

--- script.sh
+++ script.sh
@@ -6,7 +6,7 @@ cd /home/root/network
 while :
 do
     echo Reading the public ip address and save it in log file
-    curl -L checkmyip.com/ | grep dsl > log
+    curl -L ping.eu/ | grep "Your IP" > log
     git add log
     git commit -m "updating ip"
     git push -u origin master
